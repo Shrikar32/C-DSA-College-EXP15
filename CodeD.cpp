b09a7002f8e92b44cf3fa6dc6e75a3c289779cde
Shrikar32 <shrikarmujumdar32@gmail.com>
2025-09-30 00:44:52 +0530

    CodeD

---
+++ CodeD.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string.h>
+
+void print_rev(int i){
+    if(i>0) 
+    {
+        std::cout<<(i%10);
+        print_rev(i/10); 
+    }
+}
+    
+int main(){
+    int i;
+    std::cout<<"Enter the number: ";
+    std::cin>>i;
+    std::cout<<"The reverse of the number is: ";
+    print_rev(i); 
+    return 0;
+}
